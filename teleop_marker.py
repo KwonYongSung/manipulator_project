9a2896a2351f47f9488e764fef5523a88cd71c8a
KwonYongSung <112373203+KwonYongSung@users.noreply.github.com>
2022-11-22 16:54:14 +0900

    Add files via upload

--- teleop_marker.py
+++ teleop_marker.py
@@ -1,38 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2011, Willow Garage, Inc.
-# All rights reserved.
-#
-# Software License Agreement (BSD License 2.0)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of {copyright_holder} nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Will Son
 
 #aruco
 import rclpy.node
@@ -81,28 +46,6 @@ task_position_delta = 0.01  # meter
 joint_angle_delta = 0.05  # radian
 path_time = 0.5  # second
 
-usage = """
-Control Your OpenManipulator!
----------------------------
-Task Space Control:
-         (Forward, X+)
-              W                   Q (Upward, Z+)
-(Left, Y+) A     D (Right, Y-)    Z (Downward, Z-)
-              X 
-        (Backward, X-)
-
-Joint Space Control:
-- Joint1 : Increase (Y), Decrease (H)
-- Joint2 : Increase (U), Decrease (J)
-- Joint3 : Increase (I), Decrease (K)
-- Joint4 : Increase (O), Decrease (L)
-- Gripper: Increase (F), Decrease (G) | Fully Open (V), Fully Close (B)
-
-INIT : (1)
-HOME : (2)
-
-CTRL-C to quit
-"""
 
 e = """
 Communications Failed
@@ -110,21 +53,20 @@ Communications Failed
 
 
 class ArucoNode(rclpy.node.Node):
-
     def __init__(self):
         super().__init__('aruco_node')
-
-        # Declare and read parameters
-        self.declare_parameter("marker_size", .07)
+        # project parameters
+        self.declare_parameter("manipulator1_topic", "False")
+        
+        # ArucoNode / Declare and read parameters
+        self.declare_parameter("marker_size", .04)
         self.declare_parameter("aruco_dictionary_id", "DICT_5X5_250")
         self.declare_parameter("image_topic", "image_raw")
         self.declare_parameter("camera_info_topic", "camera_info")
-        #self.declare_parameter("camera_info_topic", "camera/image/camera_info")
         self.declare_parameter("camera_frame", "camera")
 
         self.marker_size = self.get_parameter("marker_size").get_parameter_value().double_value
-        dictionary_id_name = self.get_parameter(
-            "aruco_dictionary_id").get_parameter_value().string_value
+        dictionary_id_name = self.get_parameter("aruco_dictionary_id").get_parameter_value().string_value
         image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
         info_topic = self.get_parameter("camera_info_topic").get_parameter_value().string_value
         self.camera_frame = self.get_parameter("camera_frame").get_parameter_value().string_value
@@ -140,13 +82,9 @@ class ArucoNode(rclpy.node.Node):
             self.get_logger().error("valid options: {}".format(options))
 
         # Set up subscriptions
-        self.info_sub = self.create_subscription(CameraInfo,
-                                                 info_topic,
-                                                 self.info_callback,
-                                                 qos_profile_sensor_data)
+        self.info_sub = self.create_subscription(CameraInfo,info_topic,self.info_callback,qos_profile_sensor_data)
 
-        self.create_subscription(Image, image_topic,
-                                 self.image_callback, qos_profile_sensor_data)
+        self.create_subscription(Image, image_topic,self.image_callback, qos_profile_sensor_data)
 
         # Set up publishers
         self.poses_pub = self.create_publisher(PoseArray, 'aruco_poses', 10)
@@ -192,19 +130,13 @@ class ArucoNode(rclpy.node.Node):
         markers.header.stamp = img_msg.header.stamp
         pose_array.header.stamp = img_msg.header.stamp
 
-        corners, marker_ids, rejected = cv2.aruco.detectMarkers(cv_image,
-                                                                self.aruco_dictionary,
-                                                                parameters=self.aruco_parameters)
+        corners, marker_ids, rejected = cv2.aruco.detectMarkers(cv_image,self.aruco_dictionary,parameters=self.aruco_parameters)
         if marker_ids is not None:
 
             if cv2.__version__ > '4.0.0':
-                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners,
-                                                                      self.marker_size, self.intrinsic_mat,
-                                                                      self.distortion)
+                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners,self.marker_size, self.intrinsic_mat,self.distortion)
             else:
-                rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(corners,
-                                                                   self.marker_size, self.intrinsic_mat,
-                                                                   self.distortion)
+                rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(corners,self.marker_size, self.intrinsic_mat,self.distortion)
             for i, marker_id in enumerate(marker_ids):
                 pose = Pose()
                 pose.position.x = tvecs[i][0][0]
@@ -231,7 +163,11 @@ class ArucoNode(rclpy.node.Node):
                 teleop_keyboard = TeleopKeyboard()
             except Exception as e:
                 print(e)
+            
+            # /aruco_marker manipulator1_topic의 parameters를 False->True로 변경
+            self.set_parameters([rclpy.parameter.Parameter('manipulator1_topic',rclpy.Parameter.Type.STRING,'True')])
 
+			# 물건을 집기
             goal_joint_angle[0] = radians(0)
             goal_joint_angle[1] = radians(16)
             goal_joint_angle[2] = 0.00
@@ -248,15 +184,19 @@ class ArucoNode(rclpy.node.Node):
             time.sleep(1)
             
             #if marker_ids == [0]:  #0번 마커가 인식되면 특정 위치에 놓기
-            goal_joint_angle[0] = radians(180)
+            goal_joint_angle[0] = radians(0)
             goal_joint_angle[1] = radians(-10)
             goal_joint_angle[2] = 0.00
             goal_joint_angle[3] = 0.00
             pathtime = 3.0
             teleop_keyboard.send_goal_joint_space(pathtime)
             time.sleep(5)
+			
+			
             goal_joint_angle[4] = 0.005
             teleop_keyboard.send_tool_control_request()
+            
+
             
             
             
@@ -397,9 +337,7 @@ def print_present_values():
 
 
 def main():
-
     rclpy.init()
-    
     
     try:
         teleop_keyboard = TeleopKeyboard()
@@ -416,12 +354,10 @@ def main():
     teleop_keyboard.send_tool_control_request()
 
 
-        
     node = ArucoNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
-
 
     settings = None
     if os.name != 'nt':
@@ -432,111 +368,10 @@ def main():
 #    except Exception as e:
 #        print(e)
 
-    
-
-	#while 1: # 조건 : marker를 찾을 때 까지
-		#if goal_joint_angle[0] < radians(30) :
-			#goal_joint_angle[0] = prev_goal_joint_angle[0] + joint_angle_delta
-		#elif goal_joint_angle[0] > -1.3 :
-
-    try:
-        while(rclpy.ok()):
-            rclpy.spin_once(teleop_keyboard)
-            key_value = get_key(settings)
-            if key_value == 'w':
-                goal_kinematics_pose[0] = prev_goal_kinematics_pose[0] + task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'x':
-                goal_kinematics_pose[0] = prev_goal_kinematics_pose[0] - task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'a':
-                goal_kinematics_pose[1] = prev_goal_kinematics_pose[1] + task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'd':
-                goal_kinematics_pose[1] = prev_goal_kinematics_pose[1] - task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'q':
-                goal_kinematics_pose[2] = prev_goal_kinematics_pose[2] + task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'z':
-                goal_kinematics_pose[2] = prev_goal_kinematics_pose[2] - task_position_delta
-                teleop_keyboard.send_goal_task_space()
-            elif key_value == 'y':
-                goal_joint_angle[0] = prev_goal_joint_angle[0] + joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'h':
-                goal_joint_angle[0] = prev_goal_joint_angle[0] - joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'u':
-                goal_joint_angle[1] = prev_goal_joint_angle[1] + joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'j':
-                goal_joint_angle[1] = prev_goal_joint_angle[1] - joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'i':
-                goal_joint_angle[2] = prev_goal_joint_angle[2] + joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'k':
-                goal_joint_angle[2] = prev_goal_joint_angle[2] - joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'o':
-                goal_joint_angle[3] = prev_goal_joint_angle[3] + joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'l':
-                goal_joint_angle[3] = prev_goal_joint_angle[3] - joint_angle_delta
-                pathtime = path_time
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == 'f':
-                goal_joint_angle[4] = prev_goal_joint_angle[4] + 0.002
-                teleop_keyboard.send_tool_control_request()
-            elif key_value == 'g':
-                goal_joint_angle[4] = prev_goal_joint_angle[4] - 0.002
-                teleop_keyboard.send_tool_control_request()
-            elif key_value == 'v':
-                goal_joint_angle[4] = 0.01
-                teleop_keyboard.send_tool_control_request()
-            elif key_value == 'b':
-                goal_joint_angle[4] = -0.01
-                teleop_keyboard.send_tool_control_request()
-            elif key_value == '1':
-                goal_joint_angle[0] = 0.0
-                goal_joint_angle[1] = 0.0
-                goal_joint_angle[2] = 0.0
-                goal_joint_angle[3] = 0.0
-                pathtime = 5.0
-                teleop_keyboard.send_goal_joint_space(pathtime)
-            elif key_value == '2':
-                goal_joint_angle[0] = 0.0
-                goal_joint_angle[1] = -1.05
-                goal_joint_angle[2] = 0.35
-                goal_joint_angle[3] = 0.70
-                pathtime = 5.0
-                teleop_keyboard.send_goal_joint_space(pathtime)
-
-            else:
-                if key_value == '\x03':
-                    break
-                else:
-                    for index in range(0, 7):
-                        prev_goal_kinematics_pose[index] = goal_kinematics_pose[index]
-                    for index in range(0, 5):
-                        prev_goal_joint_angle[index] = goal_joint_angle[index]
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        if os.name != 'nt':
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-        teleop_keyboard.destroy_node()
-        rclpy.shutdown()
+    if os.name != 'nt':
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    teleop_keyboard.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
